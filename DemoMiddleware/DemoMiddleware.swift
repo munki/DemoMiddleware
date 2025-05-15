cd61c1da3ebb68a2fc80bf44017042afa068e8e1
Greg Neagle <gregneagle@mac.com>
2025-05-15 16:31:53 -0700

    Initial commit

---
+++ DemoMiddleware/DemoMiddleware.swift
@@ -0,0 +1,33 @@
+//
+//  DemoMiddleware.swift
+//  DemoMiddleware
+//
+//  Created by Greg Neagle on 5/14/25.
+
+import Foundation
+
+class DemoMiddleware: MunkiMiddleware {
+    func processRequest(_ request: MunkiMiddlewareRequest) -> MunkiMiddlewareRequest {
+        print("*********************************************************")
+        print(request.url)
+        if !request.headers.isEmpty {
+            print(request.headers)
+        }
+        print("*********************************************************")
+        return request
+    }
+}
+
+// MARK: dylib "interface"
+
+final class DemoMiddlewareBuilder: MiddlewarePluginBuilder {
+    override func create() -> MunkiMiddleware {
+        return DemoMiddleware()
+    }
+}
+
+/// Function with C calling style for our dylib. We use it to instantiate the Repo object and return an instance
+@_cdecl("createPlugin")
+public func createPlugin() -> UnsafeMutableRawPointer {
+    return Unmanaged.passRetained(DemoMiddlewareBuilder()).toOpaque()
+}
